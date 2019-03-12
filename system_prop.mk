d23fa34b2d9eae837ba9b8bcc226219b465edbd8
Владимир <vova.perm@hotmail.com>
2019-03-13 01:49:49 +0500

    xiaomi_msm8994-common: build fix

--- system_prop.mk
+++ system_prop.mk
@@ -201,7 +201,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.sensors.gestures=true \
     ro.qc.sdk.gestures.camera=false \
     ro.qc.sdk.camera.facialproc=false \
-    vendor.camera.hal1.packagelist=com.whatsapp,com.intsig.camscanner,com.facebook.katana,com.instagram.android,com.snapchat.android
+    vendor.camera.hal1.packagelist=com.whatsapp,com.intsig.camscanner \
+    vendor.camera.hal1.packagelist=com.facebook.katana,com.instagram.android,com.snapchat.android
 
 #property to enable user to access Google WFD settings.
 PRODUCT_PROPERTY_OVERRIDES += \
